b5d34a847a343d78558d6c59d7ab439b185ef267
Bert <zgenyuan@hotmail.com>
2018-04-12 18:08:23 +0800

    Initial commit

---
+++ BSPv2.2/Inc/bsp_digitalfloodcontrol.h
@@ -0,0 +1,15 @@
+#ifndef __bsp_digitalfloodcontrol_H
+#define __bsp_digitalfloodcontrol_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "stm32f1xx_hal.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
